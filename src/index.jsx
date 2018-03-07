ee0ffbd48ded9e1e809cacd825e6040c47195222
Estêvão Silva <tevinexpert@gmail.com>
2018-03-07 19:44:14 -0300

    Exercício 1

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,4 @@
-import react from 'react'
-import reactDOM from 'react-dom'
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-reactDOM.render(<h1>Coisa bonita</h1>, document.getElementById('app'))
+ReactDOM.render(<h1>Coisa bonita</h1>, document.getElementById('app'))
